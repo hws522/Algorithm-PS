d9276e59fb49ac9c1c71d2cc6593f225d9826d77
Hyunwoo Seo <sraphic@kakao.com>
2022-11-04 23:58:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.91 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 72.1 MB, 시간: 150.93 ms
+메모리: 56.9 MB, 시간: 95.91 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,22 +1,15 @@
 function solution(participant, completion) {
-    let 참여인원 = new Map();
-    let 성공인원 = new Map();
-    let 실패인원 = '';
+    let answer = '';
     
     participant.sort();
     completion.sort();
     
     for(let i = 0; i < participant.length; i++){
-        참여인원.set(i, participant[i]);
-        if(i !== participant.length - 1) 성공인원.set(i, completion[i]);
-    };
-    
-    for(let i = 0; i < 참여인원.size; i++){
-        if(참여인원.get(i) !== 성공인원.get(i)){
-            실패인원 = 참여인원.get(i);
+        if(participant[i] !== completion[i]){
+            answer = participant[i];
             break;
         } 
     }
     
-    return 실패인원;
+    return answer;
 }
